15ae9b1a09d7aa8ea79ccd5052e052564b5e902c
Jun So <129551078+thatcantbeso@users.noreply.github.com>
2023-10-05 21:57:14 +0000

    save

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -13,4 +13,28 @@
 #  director_id :integer
 #
 class Movie < ApplicationRecord
+
+  def director
+    d_id = self.director_id
+
+   matching_directors = Director.where({ :id => d_id })
+
+   the_director = matching_directors.at(0)
+
+  return the_director
+  end
+
+  def title_with_year
+
+    return "#{self.title} (#{self.year})"
+  end
+
+  def characters
+    my_id = self.id
+
+    matching_characters = Character.where({ :movie_id => my_id })
+
+    return matching_characters.at(0)
+  
+  end
 end
